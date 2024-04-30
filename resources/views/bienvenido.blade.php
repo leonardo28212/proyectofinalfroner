b93ba4c1ecf20da3ad399bd89f74dbaab4b79253
leonardo28212 <leonardoceron28@gmail.com>
2024-04-30 18:55:40 -0500

    first commit

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@section('titulo','sobre la moto')
+
+@section('contenido')
+<p>La XTZ 250 posee un motor de 249 cc, 4 tiempos, un cilindro, SOHC refrigerado por aire y aceite, el cual alcanza
+    20,7 HP a 8.000 rpm y 20,5 Nm de torque a 6.500 rpm. Adopta un pistón liviano de aluminio forjado y un cilindro con
+    recubrimiento plateado que permite una óptima disipación de calor. Además, incorpora un balanceador para reducir las
+    vibraciones percibidas por el conductor</p>
+@endsection
+
+
---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('titulo','contactos ')
+
+@section('contenido')
+<h1>contacto</h1>
+<p>escribenos a las lineas de atencion</p>
+@endsection
